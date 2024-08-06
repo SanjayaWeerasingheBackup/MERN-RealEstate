97ec0ffc5b2bb547d82c7bde008a7e4bed2d9be1
Sanjaya Weerasinghe <sanjayaweerasingheofficial@gmail.com>
2024-08-06 06:59:06 +0530

    Add files via upload
    from my original profile

---
+++ client/src/pages/SignUp.jsx
@@ -0,0 +1,38 @@
+import { Link } from "react-router-dom";
+
+export default function SignUp() {
+  return (
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">SignUP</h1>
+      <forum className="flex flex-col gap-4">
+        <input
+          type="text"
+          placeholder="username"
+          className="border p-3 rounderd-lg"
+          id="username"
+        />
+        <input
+          type="text"
+          placeholder="email"
+          className="border p-3 rounderd-lg"
+          id="email"
+        />
+        <input
+          type="text"
+          placeholder="password"
+          className="border p-3 rounderd-lg"
+          id="password"
+        />
+        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:oppasity-95 disabled:opacity-80">
+          SignUp
+        </button>
+      </forum>
+      <div className="flex gap-2 mt-5">
+        <p>Have an Account?</p>
+        <Link to="/sign-in">
+          <span className="text-blue-700">Sign-In</span>
+        </Link>
+      </div>
+    </div>
+  );
+}
